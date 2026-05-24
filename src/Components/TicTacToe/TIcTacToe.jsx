16b932e41ff9dcc16ae4e8f700312c90e22990b5
0Chronic <kyawn1063@gmail.com>
2026-05-24 13:06:57 +0630

    adding

--- src/Components/TicTacToe/TIcTacToe.jsx
+++ src/Components/TicTacToe/TIcTacToe.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import circle_icon from '../Assets/circle.png'
 import cross_icon from '../Assets/cross.png'
 
 import './TicTacToe.css'
 
+Let data = ["","","","","","","","","",];
+
+
+let [count,setCount] = useState(0)
+let [lock, setLock] = useState (false)
+
+
+
+
+
+
+
 
 
 const TIcTacToe = () => {
+
+
+const toggle =(e,num) => {
+  if (lock) {
+    return 0;
+  }
+  if (count%2===0){
+   e.target.innerHTML =`<img src ='${cross_icon}'>`;
+   data[num]="x"
+   setCount(++count);
+}
+}
+
+
   return (
     <div className='container'>
         <h1 className='title'>Tic Tac Toe Game In <span>React</span></h1>
         <div className='board'>
+            <div className="row1">
+                <div className="boxes"></div>
+                 <div className="boxes"></div>
+                  <div className="boxes"></div>
+            </div>
+
+              <div className="row2">
+                <div className="boxes"></div>
+                 <div className="boxes"></div>
+                  <div className="boxes"></div>
+            </div>
+
+              <div className="row3">
+                <div className="boxes"></div>
+                 <div className="boxes"></div>
+                  <div className="boxes"></div>
+            </div>
 
         </div>
 
